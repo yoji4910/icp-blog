d89f05892b2622884d86acb09fcbb79164794a40
yoji <fmn.4910@gmail.com>
2022-05-08 17:51:41 +0900

    first commit

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM node:18.1.0
 WORKDIR /usr/src/app
 
 RUN npm install -g npm@latest
-
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkModel: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
